7fbb2833a6958272b15675505a8bbd459cf8eea7
SpiderGit <gitsoza@gmail.com>
2023-02-09 13:44:20 +0530

    Fixed inconsistent styling in media queries

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Dashboard } from '@mui/icons-material';
 import { useState } from 'react'
 import { redirect, useNavigate } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
-// import './App.scss'
+import './App.scss'
 import Auth from './module/Authentication/Auth';
 import { useAppSelector } from './store/hooks'; 
 
